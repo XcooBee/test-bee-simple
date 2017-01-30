78254a61125f4df0e4363c97a830f516be724a6f
Oscar Rene Flores Presidente <oscar.rene1989@gmail.com>
2017-01-30 07:43:03 -0600

    Changes the test bee so it reads data from a file

--- src/handler.js
+++ src/handler.js
@@ -2,5 +2,15 @@
 
 module.exports.flight = (services, data, callback) => {
     services.log("Look at me, I'm buzzing", "info");
-    callback(null);
+    services.log(`${data.user_data.first_name} ${data.user_data.last_name}`, "info");
+
+    let fileData = "";
+    services.readStream.on("data", (chunk) => {
+        fileData += chunk;
+    });
+
+    services.readStream.on("end", () => {
+        services.log(`${fileData}`, "info");
+        // callback(null, "Success");
+    });
 };
