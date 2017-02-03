771db4f33cf947d406dc61030b4cbd3a1470b22b
Oscar Rene Flores Presidente <oscar.rene1989@gmail.com>
2017-02-03 10:52:13 -0600

    Updates testing code

--- src/handler.js
+++ src/handler.js
@@ -1,16 +1,26 @@
 "use strict";
 
 module.exports.flight = (services, data, callback) => {
-    services.log("Look at me, I'm buzzing", "info");
-    services.log(`${data.user_data.first_name} ${data.user_data.last_name}`, "info");
+    // services.log("Look at me, I'm buzzing", "info");
+    // services.log(`${data.user_data.first_name} ${data.user_data.last_name}`, "info");
 
-    let fileData = "";
-    services.readStream.on("data", (chunk) => {
-        fileData += chunk;
-    });
+    // let fileData = "";
+    // services.readStream.on("data", (chunk) => {
+    //     fileData += chunk;
+    // });
+    // services.readStream.on("end", () => {
+    //     services.log(`${fileData}`, "info");
+    //     callback(null, "Success");
+    // });
 
-    services.readStream.on("end", () => {
-        services.log(`${fileData}`, "info");
+
+    const writeStream = services.writeStreamManager().getWriteStream("hello.txt", "output");
+    writeStream.on("finish", () => {
+        services.mail("sender", "awesome-template", { favColor: "green" });
         callback(null, "Success");
+    });
+    writeStream.end();
+    writeStream.write("Hello World!", "utf8", () => {
+        writeStream.end();
     });
 };
