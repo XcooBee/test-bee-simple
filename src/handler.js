52c16b8aefe8d0d316ab962db6a046f5412a6d9e
Oscar Rene Flores Presidente <oscar.rene1989@gmail.com>
2017-02-05 21:20:05 -0600

    Fixes a typo, writeStreamManager called as function

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports.flight = (services, data, callback) => {
-    const writeStream = services.writeStreamManager().getWriteStream("hello.txt", "output");
+    const writeStream = services.writeStreamManager.getWriteStream("hello.txt", "output");
     writeStream.on("finish", () => {
         services.mail("sender", "awesome-template", { favColor: "green" });
         callback(null, "Success");
