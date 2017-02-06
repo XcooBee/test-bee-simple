b9a0abb9139905eb1ec1bcafe85b8126b5118486
Oscar Rene Flores Presidente <oscar.rene1989@gmail.com>
2017-02-05 21:09:51 -0600

    Refactors bee to retrieve a write stream through writeStreamManager

--- src/handler.js
+++ src/handler.js
@@ -1,19 +1,6 @@
 "use strict";
 
 module.exports.flight = (services, data, callback) => {
-    // services.log("Look at me, I'm buzzing", "info");
-    // services.log(`${data.user_data.first_name} ${data.user_data.last_name}`, "info");
-
-    // let fileData = "";
-    // services.readStream.on("data", (chunk) => {
-    //     fileData += chunk;
-    // });
-    // services.readStream.on("end", () => {
-    //     services.log(`${fileData}`, "info");
-    //     callback(null, "Success");
-    // });
-
-
     const writeStream = services.writeStreamManager().getWriteStream("hello.txt", "output");
     writeStream.on("finish", () => {
         services.mail("sender", "awesome-template", { favColor: "green" });
