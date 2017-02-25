822ca6edebc738d8e7a95a54dc827b2464b651f7
Oscar Rene Flores Presidente <oscar.rene1989@gmail.com>
2017-02-24 23:17:06 -0600

    Makes the bee to output more than one file

--- src/handler.js
+++ src/handler.js
@@ -2,6 +2,8 @@
 
 module.exports.flight = (services, data, callback) => {
     const writeStream = services.writeStreamManager.getWriteStream("hello.txt", "output");
+    const anotherWriteStream = services.writeStreamManager.getWriteStream("hello2.txt", "output");
+
     writeStream.on("finish", () => {
         services.mail("sender", "awesome-template", { favColor: "green" });
         callback(null, "Success");
@@ -9,5 +11,9 @@ module.exports.flight = (services, data, callback) => {
 
     writeStream.write("Hello World!", "utf8", () => {
         writeStream.end();
+    });
+
+    anotherWriteStream.write("This is another output", "utf8", () => {
+        anotherWriteStream.end();
     });
 };
